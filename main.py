2be112a69a141f73db0e327e1fe6764c4a6303e4
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-04-23 11:14:33 +0530

    tkinter

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ window.title("First Tkinter Program")
 window.minsize(width=500,height=200)
 
 #label in tkinter
-my_label =tkinter.Label(text="I'm small label of tkinter class",font=("Arial",14,"bold"))
+my_label =tkinter.Label(text="User Name ....",font=("Arial",14,"bold"))
 my_label.pack(expand=True)
+# na
+name=" ...."
+my_label.config(text=f"User Name : {name}")
+
+def close_me():
+    name=inp__.get()
+    my_label.config(text=f"Your Name is : {name}")
+
+#buttons
+button=tkinter.Button(text="Close Window", command=close_me)
+button.pack()
+inp__ =tkinter.Entry(width=25)
+inp__.pack()
 
 #windwo's main llop
 window.mainloop()
